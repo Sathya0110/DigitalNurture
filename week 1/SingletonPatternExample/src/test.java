a1d7afb02eab89484cbf299e9f5701378d766152
Sathya0110 <115555820+Sathya0110@users.noreply.github.com>
2026-07-01 21:17:16 +0530

    Delete week 1 directory

--- week 1/SingletonPatternExample/src/test.java
+++
@@ -1,7 +0,0 @@
-class test{
-    public static void main(String[]args){
-        Logger l1 = Logger.getLogger("Garnet", 1);
-        Logger l2 = Logger.getLogger("Giri", 2);
-        System.out.print(l1 == l2);
-    }
-}
